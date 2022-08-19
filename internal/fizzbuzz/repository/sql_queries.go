8f6920590ed2d5afc08d846ab13af16fa58ea278
Pierre-Yves BOISBUNON <boisbu@gmail.com>
2022-08-19 11:24:11 +0200

    cleanup queries

--- internal/fizzbuzz/repository/sql_queries.go
+++ internal/fizzbuzz/repository/sql_queries.go
@@ -1,12 +1,28 @@
 package repository
 
 const (
-	insertRecord = `INSERT INTO fizzbuzz.stats(int1, int2, max_limit, str1, str2) 
-					VALUES ($1, $2, $3, $4, $5);`
+	insertRecord = `
+					INSERT INTO 
+						fizzbuzz.stats(int1, int2, max_limit, str1, str2) 
+					VALUES 
+						($1, $2, $3, $4, $5);`
 	getStats = `
-					SELECT int1, int2, max_limit, str1, str2, COUNT(*) AS count
-					FROM fizzbuzz.stats
-					GROUP BY int1, int2, max_limit, str1, str2
-					ORDER BY count DESC
+					SELECT
+						int1,
+						int2,
+						max_limit,
+						str1,
+						str2,
+						COUNT(*) AS count
+					FROM
+						fizzbuzz.stats
+					GROUP BY 
+						int1,
+						int2,
+						max_limit,
+						str1,
+						str2
+					ORDER BY
+						count DESC
 					LIMIT 1;`
 )
